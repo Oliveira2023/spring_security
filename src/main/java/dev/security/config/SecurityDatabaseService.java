4f25bffc2ae73966ce073d7f511b01425cf61ab0
Luciano Oliveira <luciano.oliveira96@yahoo.com.br>
2025-02-27 21:58:36 -0300

    update 270225 ok security com usuarios em memoria e roles.

---
+++ src/main/java/dev/security/config/SecurityDatabaseService.java
@@ -0,0 +1,19 @@
+package dev.security.config;
+
+//import dev.security.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+//@Component
+//public class SecurityDatabaseService implements UserDetailsService {
+//    @Autowired
+//    private UserRepository userRepository;
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//
+//        return null;
+//    }
+//}
